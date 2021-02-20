b437b612ffabe572604d58db30dd4bf7d67a729a
marc.luettecke <marc.luettecke@uni-konstanz.de>
2021-02-20 17:10:13 +0100

    added material sidebar navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,21 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './modules/angular-material.module';
-import { NavbarComponent } from './components/landing-page/misc/navbar/navbar.component';
+import { MainNavComponent } from './components/landing-page/misc/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    NavbarComponent
-  ],
+    MainNavComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
