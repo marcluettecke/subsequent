db1ff43f7f78c20ce0de3057d0678ef91578e37c
Jonas Ostergaard <info@jonas-ostergaard.de>
2021-05-13 22:52:53 +0200

    implemented first prototype of the network graphic, first version of automatic drawing of cables implemented (under construction)

--- src/app/components/landing-page/feature-list/feature-list.component.ts
+++ src/app/components/landing-page/feature-list/feature-list.component.ts
@@ -1,19 +1,22 @@
 import {
   Component,
   ElementRef,
-  NgZone,
   OnInit,
   Renderer2,
   ViewChild,
 } from "@angular/core";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import {
   BreakpointObserver,
   Breakpoints,
   BreakpointState,
 } from "@angular/cdk/layout";
 import { FeatureModalComponent } from "./feature-modal/feature-modal.component";
+import {
+  FeatureNetworkScreen,
+  FeatureNetworkCable,
+} from "../models/LandingPage";
 
 @Component({
   selector: "app-feature-list",
@@ -24,24 +27,96 @@ export class FeatureListComponent implements OnInit {
   @ViewChild("networkGraphicCanvas", { static: false })
   networkGraphicCanvas: ElementRef;
 
-  networkScreens: Array<any> = [
+  @ViewChild("networkGraphicWrapper", { static: false })
+  networkGraphicWrapper: ElementRef;
+
+  networkScreens: Array<FeatureNetworkScreen> = [
     {
       id: 0,
-      // x: this.canvasWidth / 4,
-      // y: this.canvasHeight / 5,
-      // width: this.canvasWidth / 5,
+      xPerc: 0.15,
+      yPerc: 0.3,
+      widthPerc: 0.15,
+      pos: {
+        x: null,
+        y: null,
+      },
+    },
+    {
+      id: 1,
+      xPerc: 0.15,
+      yPerc: 0.75,
+      widthPerc: 0.12,
+      pos: {
+        x: null,
+        y: null,
+      },
+    },
+    {
+      id: 2,
+      xPerc: 0.35,
+      yPerc: 0.5,
+      widthPerc: 0.14,
+      pos: {
+        x: null,
+        y: null,
+      },
     },
     {
       id: 3,
-      // x: (this.canvasWidth / 4) * 3,
-      // y: (this.canvasHeight / 5) * 3,
-      // width: this.canvasWidth / 7,
+      xPerc: 0.8,
+      yPerc: 0.7,
+      widthPerc: 0.13,
+      pos: {
+        x: null,
+        y: null,
+      },
+    },
+    {
+      id: 4,
+      xPerc: 0.7,
+      yPerc: 0.3,
+      widthPerc: 0.15,
+      pos: {
+        x: null,
+        y: null,
+      },
+    },
+    {
+      id: 5,
+      xPerc: 0.65,
+      yPerc: 0.5,
+      widthPerc: 0.13,
+      pos: {
+        x: null,
+        y: null,
+      },
     },
   ];
 
-  networkCables: Array<any> = [];
+  networkCables: Array<FeatureNetworkCable> = [];
 
-  canvasDrawed: boolean = false;
+  mainCableAnchorPoints = {
+    left: {
+      pos1: {
+        xPerc: 0.45, // initially % value
+        yPerc: 0.0,
+      },
+      pos2: {
+        xPerc: 0.45, // initially % value
+        yPerc: 0.25,
+      },
+    },
+    right: {
+      pos1: {
+        xPerc: 0.55, // initially % value
+        yPerc: 0.0,
+      },
+      pos2: {
+        xPerc: 0.55, // initially % value
+        yPerc: 0.25,
+      },
+    },
+  };
 
   canvasWidth: number = 0;
 
@@ -60,45 +135,158 @@ export class FeatureListComponent implements OnInit {
     Breakpoints.XSmall
   );
 
+  isSmallSubscription: Subscription;
+
+  isSmall: Observable<BreakpointState> = this.breakpointObserver.observe(
+    Breakpoints.Small
+  );
+
+  isMediumSubscription: Subscription;
+
+  isMedium: Observable<BreakpointState> = this.breakpointObserver.observe(
+    Breakpoints.Medium
+  );
+
+  isLargeSubscription: Subscription;
+
+  isLarge: Observable<BreakpointState> = this.breakpointObserver.observe(
+    Breakpoints.Large
+  );
+
+  isXLargeSubscription: Subscription;
+
+  isXLarge: Observable<BreakpointState> = this.breakpointObserver.observe(
+    Breakpoints.XLarge
+  );
+
   constructor(
     private matDialog: MatDialog,
     private readonly breakpointObserver: BreakpointObserver,
-    private renderer2: Renderer2,
-    private ngZone: NgZone
+    private renderer2: Renderer2
   ) {}
 
-  ngOnInit(): void {}
-
-  ngAfterViewChecked(): void {
-    if (this.canvasWidth === 0) {
-      this.canvasWidth = this.networkGraphicCanvas.nativeElement.width.baseVal.value;
-      this.canvasHeight = this.networkGraphicCanvas.nativeElement.height.baseVal.value;
-    } else if (!this.canvasDrawed) {
-      console.log("this.canvasWidth: ", this.canvasWidth);
-      console.log("this.canvasHeight: ", this.canvasHeight);
-      this.ngZone.run(() => {
-        this.drawNetwork();
-      });
-    }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.drawNetwork();
+      const resizeFunction = () => {
+        setTimeout(() => {
+          this.drawNetwork();
+        }, 300);
+      };
+      this.isSmallSubscription = this.isSmall.subscribe(resizeFunction);
+      this.isMediumSubscription = this.isMedium.subscribe(resizeFunction);
+      this.isLargeSubscription = this.isLarge.subscribe(resizeFunction);
+      this.isXLargeSubscription = this.isXLarge.subscribe(resizeFunction);
+    }, 300);
   }
 
+  ngOnDestroy(): void {
+    this.isSmallSubscription.unsubscribe();
+    this.isMediumSubscription.unsubscribe();
+    this.isLargeSubscription.unsubscribe();
+    this.isXLargeSubscription.unsubscribe();
+  }
+
+  ngAfterViewChecked(): void {}
+
+  /**
+   * recalculates all fields of all feature screens
+   */
+  recalculateScreens() {
+    this.networkScreens.forEach((screen, index) => {
+      this.networkScreens[index].pos = {
+        x: this.canvasWidth * screen.xPerc,
+        y: this.canvasHeight * screen.yPerc,
+      };
+      this.networkScreens[index].width = this.canvasWidth * screen.widthPerc;
+      this.networkScreens[
+        index
+      ].style = `left:${screen.pos.x}px;top:${screen.pos.y}px;width:${screen.width}px;`;
+    });
+  }
+
+  /**
+   * recalculates all fields of all cables
+   */
+  recalculateCables() {
+    this.networkCables = [
+      {
+        class: "network-cable main-cable left",
+        pos1: {
+          x: this.canvasWidth * this.mainCableAnchorPoints.left.pos1.xPerc,
+          y: this.canvasHeight * this.mainCableAnchorPoints.left.pos1.yPerc,
+        },
+        pos2: {
+          x: this.canvasWidth * this.mainCableAnchorPoints.left.pos2.xPerc,
+          y: this.canvasHeight * this.mainCableAnchorPoints.left.pos2.yPerc,
+        },
+      },
+      {
+        class: "network-cable main-cable right",
+        pos1: {
+          x: this.canvasWidth * this.mainCableAnchorPoints.right.pos1.xPerc,
+          y: this.canvasHeight * this.mainCableAnchorPoints.right.pos1.yPerc,
+        },
+        pos2: {
+          x: this.canvasWidth * this.mainCableAnchorPoints.right.pos2.xPerc,
+          y: this.canvasHeight * this.mainCableAnchorPoints.right.pos2.yPerc,
+        },
+      },
+    ];
+    this.networkScreens.forEach((screen) => {
+      let mCableIndex = 1;
+      let sideClass = "right";
+      if (screen.pos.x && screen.pos.x < this.canvasWidth / 2) {
+        mCableIndex = 0;
+        sideClass = "left";
+      }
+      this.networkCables.push({
+        class: `network-cable ${sideClass}`,
+        screenID: screen.id,
+        pos1: this.networkCables[mCableIndex].pos2,
+        pos2: screen.pos,
+      });
+    });
+  }
+
+  /**
+   * draws or refreshes the feature network
+   */
   drawNetwork() {
-    this.canvasDrawed = true;
-    // draw network SCREENS:
-    this.networkScreens.push({});
-    this.networkScreens.push({});
+    this.canvasWidth = this.networkGraphicCanvas.nativeElement.width.baseVal.value;
+    this.canvasHeight = this.networkGraphicCanvas.nativeElement.height.baseVal.value;
+    if (this.canvasWidth === 0 || this.canvasHeight === 0) {
+      setTimeout(() => {
+        this.drawNetwork();
+      }, 300);
+    }
+    // setup network SCREENS:
+    this.recalculateScreens();
     console.log("this.networkScreens: ", this.networkScreens);
+
+    // delete old CABLES:
+    const childElements = this.networkGraphicCanvas.nativeElement.children;
+    for (let i = 0; i < childElements.length; i++) {
+      this.renderer2.removeChild(
+        this.networkGraphicCanvas.nativeElement,
+        childElements[i]
+      );
+    }
     // draw network CABLES:
-    const line1: SVGLineElement = this.renderer2.createElement(
-      "line",
-      this.networkGraphicCanvas.nativeElement.namespaceURI
-    );
-    line1.setAttributeNS(null, "x1", "0");
-    line1.setAttributeNS(null, "x2", "200");
-    line1.setAttributeNS(null, "y1", "0");
-    line1.setAttributeNS(null, "y2", "200");
-    line1.setAttributeNS(null, "class", "networkCable left");
-    this.networkGraphicCanvas.nativeElement.appendChild(line1);
+    this.recalculateCables();
+    this.networkCables.forEach((cable) => {
+      const line1: SVGLineElement = this.renderer2.createElement(
+        "line",
+        this.networkGraphicCanvas.nativeElement.namespaceURI
+      );
+      line1.setAttributeNS(null, "x1", `${cable.pos1.x}`);
+      line1.setAttributeNS(null, "x2", `${cable.pos2.x}`);
+      line1.setAttributeNS(null, "y1", `${cable.pos1.y}`);
+      line1.setAttributeNS(null, "y2", `${cable.pos2.y}`);
+      line1.setAttributeNS(null, "class", cable.class);
+      this.networkGraphicCanvas.nativeElement.appendChild(line1);
+    });
+    console.log("this.networkCables: ", this.networkCables);
   }
 
   openModal(event: MouseEvent, index: number) {
--- src/app/components/landing-page/models/LandingPage.ts
+++ src/app/components/landing-page/models/LandingPage.ts
@@ -9,3 +9,29 @@ export interface IPersonalDescription {
   role: string;
   random: string;
 }
+
+export interface FeatureNetworkScreen {
+  id: number;
+  xPerc: number;
+  yPerc: number;
+  widthPerc: number;
+  pos: {
+    x: number | null;
+    y: number | null;
+  };
+  width?: number;
+  style?: string;
+}
+
+export interface FeatureNetworkCable {
+  screenID?: number;
+  pos1: {
+    x: number | null;
+    y: number | null;
+  };
+  pos2: {
+    x: number | null;
+    y: number | null;
+  };
+  class: string;
+}
